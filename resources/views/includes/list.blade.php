8e50c1db971e792eeb8c124981b1495fa7021c2c
kstdbrv <kstdbrv1@gmail.com>
2020-07-06 16:30:08 +0300

    add list

--- resources/views/includes/list.blade.php
+++ resources/views/includes/list.blade.php
@@ -1,6 +1,52 @@
 <section class="list">
+    <div class="container">
+        <div class="profile">
+            <ul>
+                <li class="profile__fild">
+                    <div class="profile__number">365</div>
+                </li>
+                <li class="profile__name">Алексей Коровкин</li>
+                <li class="profile__city">Череповец</li>
+                <li class="profile__age">25 лет</li>
+                <li class="profile__specialty">Переводчик</li>
+                <li><a class="profile__write" href="#">Написать</a></li>
+                <li><a class="profile__call" href="#">Позвонить</a></li>
+            </ul>
+        </div>
+        <div class="profile">
+            <ul>
+                <li class="profile__fild">
+                    <div class="profile__number">365</div>
+                </li>
+                <li class="profile__name">Алексей Коровкин</li>
+                <li class="profile__city">Череповец</li>
+                <li class="profile__age">25 лет</li>
+                <li class="profile__specialty">Переводчик</li>
+                <li><a class="profile__write" href="#">Написать</a></li>
+                <li><a class="profile__call" href="#">Позвонить</a></li>
+            </ul>
+        </div>
+        <div class="profile">
+            <ul>
+                <li class="profile__fild">
+                    <div class="profile__number">365</div>
+                </li>
+                <li class="profile__name">Алексей Коровкин</li>
+                <li class="profile__city">Череповец</li>
+                <li class="profile__age">25 лет</li>
+                <li class="profile__specialty">Переводчик</li>
+                <li><a class="profile__write" href="#">Написать</a></li>
+                <li><a class="profile__call" href="#">Позвонить</a></li>
+            </ul>
+        </div>
 
 
 
+
+    </div>
 </section>
+
+
+
+
 
