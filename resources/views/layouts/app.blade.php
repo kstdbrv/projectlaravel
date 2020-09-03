20d50e25f0f9bdaa5e64e3d4fb434cd2671df9b9
kstdbrv <kstdbrv1@gmail.com>
2020-09-03 16:29:09 +0300

    changed font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/app.css">
     <title>@yield('title')</title>
 </head>
